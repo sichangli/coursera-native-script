e0189647f990aa007058ff7b6a9d7b8ead5ac8e5
Sichang Li <sichang_li@apple.com>
2020-07-05 23:57:16 -0700

    fix gitignore

--- app/menu/menu.component.ts
+++ app/menu/menu.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DishService } from '../services/dish.service';
+import { Component, OnInit, Inject } from "@angular/core";
+import { Dish } from "../shared/dish";
+import { DishService } from "../services/dish.service";
 
 @Component({
-  selector: 'app-menu',
+  selector: "app-menu",
   moduleId: module.id,
-  templateUrl: './menu.component.html',
-//   styleUrls: ['./menu.component.css']
+  templateUrl: "./menu.component.html"
+  // styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
   dishes: Dish[];
   errMess: string;
 
-  constructor(private dishService: DishService,
-    @Inject('baseURL') private BaseURL: string) { }
+  constructor(
+    private dishService: DishService,
+    @Inject("baseURL") private BaseURL: string
+  ) {}
 
   ngOnInit() {
-    this.dishService.getDishes()
-      .subscribe(dishes => this.dishes = dishes,
-        errmess => this.errMess = <any>errmess);
+    this.dishService.getDishes().subscribe(
+      dishes => (this.dishes = dishes),
+      errmess => (this.errMess = <any>errmess)
+    );
   }
-
 }
