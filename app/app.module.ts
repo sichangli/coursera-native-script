3b4eb6fff42d523ecfa33fcd90e6218477384976
Sichang Li <sichang_li@apple.com>
2020-07-05 23:44:26 -0700

    NativeScript Angular

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,15 +3,16 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
 
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 // Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
-// import { NativeScriptHttpModule } from "nativescript-angular/http";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
+import { HttpClientModule } from "@angular/common/http";
+import { MenuComponent } from "./menu/menu.component";
+import { baseURL } from "./shared/baseurl";
+import { DishService } from "./services/dish.service";
+import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
 
 @NgModule({
     bootstrap: [
@@ -19,15 +20,18 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpModule,
+        HttpClientModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        MenuComponent
     ],
     providers: [
-        ItemService
+        {provide: 'baseURL', useValue: baseURL},
+        DishService,
+        ProcessHTTPMsgService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
