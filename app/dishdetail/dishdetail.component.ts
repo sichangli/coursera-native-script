a26d5f9099b0f91c2a4c063f8c580ed4efa244ce
Sichang Li <sichang_li@apple.com>
2020-07-07 22:47:13 -0700

    Dialogs and Toasts

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -7,6 +7,7 @@ import { RouterExtensions } from "nativescript-angular/router";
 import { switchMap } from "rxjs/operators";
 import { FavoriteService } from "~/services/favorite.service";
 import { TNSFontIconService } from "nativescript-ngx-fonticon";
+import { Toasty } from "nativescript-toasty";
 
 @Component({
   selector: "app-dishdetail",
@@ -57,8 +58,8 @@ export class DishdetailComponent implements OnInit {
     if (!this.favorite) {
       console.log("Adding to Favorites", this.dish.id);
       this.favorite = this.favoriteservice.addFavorite(this.dish.id);
-      // const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
-      // toast.show();
+      const toast = new Toasty("Added Dish " + this.dish.id, "short", "bottom");
+      toast.show();
     }
   }
 
